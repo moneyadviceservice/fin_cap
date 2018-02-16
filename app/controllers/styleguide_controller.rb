95dff9c1c6eac70b537af9b5c4277d26affa7971
Margo Urey <margo@margonline.co.uk>
2018-02-16 11:50:00 +0000

    refactor menu view

--- app/controllers/styleguide_controller.rb
+++ app/controllers/styleguide_controller.rb
@@ -1,7 +1,24 @@
 class StyleguideController < ApplicationController
+  before_action :components, :links
   layout 'styleguide'
 
   def show
     render params[:page]
+  end
+
+  private
+
+  def components
+    @components = {
+      'Teaser box' => 'teaser',
+      'Hero image' => 'hero',
+      'Bordered box' => 'borderedbox',
+      'Latest news' => 'latestnews',
+      'Download box' => 'downloads'
+    }
+  end
+
+  def links
+    @links = %w[Layout Typography Links Lists Colours]
   end
 end
