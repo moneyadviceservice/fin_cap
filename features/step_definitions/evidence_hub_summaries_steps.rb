775549722e150f345d4cec471cd3a197b8c700df
Tomas D'Stefano <tomas_stefano@successoft.com>
2018-02-23 15:46:55 +0000

    Add evidence hub summaries site prism

--- features/step_definitions/evidence_hub_summaries_steps.rb
+++ features/step_definitions/evidence_hub_summaries_steps.rb
@@ -1,5 +1,8 @@
 Given("I entered into the Insight page {string}") do |title|
+  evidence_hub_summary_page.load(page_type: 'insights', slug: title.parameterize)
 end
 
 Then("I should see the content") do |table|
+  binding.pry
+  table
 end
---
+++ features/support/ui/pages/evidence_hub_summary.rb
@@ -0,0 +1,7 @@
+module UI
+  module Pages
+    class EvidenceHubSummary < UI::Page
+      set_url "/en/{/page_type}/{/slug}"
+    end
+  end
+end
