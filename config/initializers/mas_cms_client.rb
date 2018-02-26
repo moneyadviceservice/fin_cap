932062f26592228f26444f9de4e138d7fd61f48a
Jones Agyemang <mightyj@hotmail.co.uk>
2018-02-26 14:16:18 +0000

    Adds MAS CMS API client gem

---
+++ config/initializers/mas_cms_client.rb
@@ -0,0 +1,8 @@
+Mas::Cms::Client.config do |c|
+  c.timeout =  ENV['HTTP_REQUEST_TIMEOUT'].to_i
+  c.open_timeout =  ENV['HTTP_REQUEST_TIMEOUT'].to_i
+  c.host =  ENV['FINCAP_CMS_URL']
+  c.retries = 1
+  c.cache = Rails.cache
+  c.cache_gets  = false
+end
