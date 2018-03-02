71536f94f8227d086cdbdeb6086701f9960c6d70
Tomas D'Stefano <tomas_stefano@successoft.com>
2018-03-02 14:07:44 +0000

    Add VCR to feature testing.
    I decided to use the same config that is using on RSpec
    to avoid duplication config across RSpec and Cucumber.
    
    With this configuration, any request to CMS from the
    feature tests will record on cassettes and use it.

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,6 +6,7 @@
 # files.
 
 require 'cucumber/rails'
+require Rails.root.join('spec', 'support', 'vcr')
 
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
