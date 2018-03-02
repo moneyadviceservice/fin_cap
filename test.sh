6e711ca21f3dd2129299892853eefc74dc38afb5
Tomas D'Stefano <tomas_stefano@successoft.com>
2018-03-02 12:30:57 +0000

    Add bowndler to test.sh
    The app was failing the test.sh due to the lack of bowndler install.
    Added and everything passes

--- test.sh
+++ test.sh
@@ -6,6 +6,7 @@ export RAILS_ENV=test
 export BUNDLE_WITHOUT=development
 
 bundle install --quiet
+bundle exec bowndler install
 bundle exec brakeman -q --no-pager --ensure-latest
 bundle exec rubocop .
 bundle exec rspec -f progress
