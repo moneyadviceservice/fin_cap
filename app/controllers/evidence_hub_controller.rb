4dcf2830280cb11b6c2d720e8c58a14ab3cc41c0
Margo Urey <margo@margonline.co.uk>
2018-03-21 18:48:27 +0000

    Instantiate evidence hub search form to use with:
    - custom form builder
    - translations for the filter option text

--- app/controllers/evidence_hub_controller.rb
+++ app/controllers/evidence_hub_controller.rb
@@ -6,13 +6,64 @@ class EvidenceHubController < ApplicationController
       params: search_params
     )
     @evidence_summaries = EvidenceSummary.map(documents)
+    @search_form = EvidenceHubSearchForm.new
   end
 
   private
 
   def search_params
-    { document_type: DOCUMENT_TYPES }.tap do |hash|
+    { 
+      locale: params[:locale] =~ /en|cy/ ? params[:locale] : 'en',
+      document_type: DOCUMENT_TYPES 
+    }.tap do |hash|
       hash[:keyword] = params.require(:keyword) if params[:keyword].present?
+    end.merge(format_params)
+  end
+
+  def filter_params
+    params.fetch(:evidence_hub_search_form, {}).permit(
+      :years_of_publication,
+      client_groups: [],
+      topics: [],
+      countries_of_delivery: []
+    )
+  end
+
+  def format_params
+    return {} unless params[:evidence_hub_search_form].present?
+
+    format_filters(
+      filter_params.to_h.reject {|_,v| empty_filter?(v) }
+    )
+  end
+
+  def format_filters(filters)
+    { blocks: blocks(filters).flatten }
+  end
+
+  def blocks(filters)
+    filters.map do |id, values|
+      if values.is_a?(Array)
+        values.reject(&:blank?).map {|value| identifier_hash(id, value) }
+      else
+        identifier_hash(id, values)
+      end
     end
+  end
+
+  def identifier_hash(id, value)
+    { identifier: id, value: value }
+  end
+
+  def empty_filter?(value)
+    empty_string?(value) || empty_array?(value)
+  end
+
+  def empty_string?(value)
+    value.is_a?(String) && value.empty?
+  end
+
+  def empty_array?(value)
+    value.is_a?(Array) && value.one? && value.first.empty?
   end
 end
---
+++ app/presenters/evidence_hub_search_form_presenter.rb
@@ -0,0 +1,15 @@
+class EvidenceHubSearchFormPresenter < BasePresenter
+  def translate_field(field)
+    object.class.human_attribute_name(field)
+  end
+
+  def filter_options(field)
+    translate_field(field).map do |option|
+      FilterOption.new(option)
+    end
+  end
+
+  def class_name
+    object.class.name.underscore
+  end
+end
