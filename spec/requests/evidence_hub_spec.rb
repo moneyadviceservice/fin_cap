c5131e64b51fc2a48f9d1c3867c01bad76b8e368
Margo Urey <margo@margonline.co.uk>
2018-03-29 09:54:50 +0100

    format filter params from form

--- spec/requests/evidence_hub_spec.rb
+++ spec/requests/evidence_hub_spec.rb
@@ -1,11 +1,45 @@
 RSpec.describe 'Evidence Hub Summaries', type: :request do
   describe 'GET /evidence_hub' do
-    before do
-      get '/en/evidence_hub'
+    
+    context 'arriving on the Evidence Hub page' do
+      before { get '/en/evidence_hub' }
+      
+      it 'responds successful' do
+        expect(response).to be_ok
+      end
     end
 
-    it 'responds successful' do
-      expect(response).to be_ok
+    context 'searching with filters' do
+      let(:filters) do
+        {
+          evidence_hub_search_form:
+            {
+              'years_of_publication' => 'All years',
+              'client_groups'=>['', 'Young people (12 - 16)', 'Parents/families']
+            }
+        }
+      end
+
+      it 'formats the filter params correctly' do
+        expect(Mas::Cms::Document).to receive(:all)
+          .with(params: formatted_filters)
+          .and_return([])
+
+        get "/en/evidence_hub/?#{filters.to_query}"
+      end
     end
+  end
+
+  def formatted_filters
+    { 
+      locale: 'en',
+      document_type: ['Insight'],
+      blocks:
+      [
+        { identifier: 'years_of_publication', value: 'All years' },
+        { identifier: 'client_groups', value: 'Young people (12 - 16)' },
+        { identifier: 'client_groups', value: 'Parents/families' }
+      ]
+    }
   end
 end
