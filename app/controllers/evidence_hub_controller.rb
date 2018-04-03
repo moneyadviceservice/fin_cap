5ce2f26c8c30904a8d62bc3c331f37c09bfe8682
Margo Urey <margo@margonline.co.uk>
2018-04-03 17:15:03 +0100

    refactor: use translations for filters
    - rename evidence_hub_search_form to evidence_summary_search_form
    - remove unnecessary parsing from filter parser
    - update tests

--- app/controllers/evidence_hub_controller.rb
+++ app/controllers/evidence_hub_controller.rb
@@ -2,31 +2,25 @@ class EvidenceHubController < ApplicationController
   DOCUMENT_TYPES = ['Insight'].freeze
 
   def index
+    @search_form = EvidenceSummarySearchForm.new(search_form_params)
+
     documents = Mas::Cms::Document.all(
-      params: { document_type: DOCUMENT_TYPES }
+      params: parse_filter_params
     )
     @evidence_summaries = EvidenceSummary.map(documents)
-
-    @search_form = EvidenceHubSearchForm.new(search_form_params)
   end
 
   private
 
   def search_form_params
-    { 
-      document_type: DOCUMENT_TYPES 
-    }.tap do |hash|
-      hash[:keyword] = params.require(:keyword) if params[:keyword].present?
-    end.merge(filter_params)
-
+    {
+      document_type: DOCUMENT_TYPES
+    }.merge(evidence_summary_search_form_params)
   end
 
-  def search_params
-    search_form_params.merge(parse_filter_params)
-  end
-
-  def filter_params
-    params.fetch(:evidence_hub_search_form, {}).permit(
+  def evidence_summary_search_form_params
+    params.fetch(:evidence_summary_search_form, {}).permit(
+      :keyword,
       :years_of_publication,
       client_groups: [],
       topics: [],
@@ -34,8 +28,18 @@ class EvidenceHubController < ApplicationController
     )
   end
 
+  def filter_params
+    evidence_summary_search_form_params.select do |k, _|
+      filter_keys.include?(k)
+    end
+  end
+
   def parse_filter_params
-    return {} unless params[:evidence_hub_search_form].present?
+    return {} if params[:evidence_summary_search_form].blank?
     FilterParser.parse(filter_params.to_h)
+  end
+
+  def filter_keys
+    @search_form.checkbox_filters + @search_form.radio_button_filters
   end
 end
--- app/models/evidence_summary_search_form.rb
+++ app/models/evidence_summary_search_form.rb
@@ -11,4 +11,16 @@ class EvidenceSummarySearchForm
   def class_name
     self.class.name.underscore
   end
+
+  def checkbox_filters
+    I18n.t('evidence_hub.filters.checkbox')
+  end
+
+  def radio_button_filters
+    I18n.t('evidence_hub.filters.radio_button')
+  end
+
+  def filter_title(filter)
+    filter.split('_').map(&:singularize).join(' ').capitalize
+  end
 end
