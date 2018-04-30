13b469a19a07475652affff2611b2721ee7cf008
osharpe <oliver.sharpe@moneyadviceservice.org.uk>
2018-04-30 16:03:26 +0100

    Update test

--- test.sh
+++ test.sh
@@ -8,10 +8,9 @@ export BUNDLE_WITHOUT=development
 
 bundle install
 npm install
-
 bundle exec bowndler update
-
 bundle exec rake db:drop db:create db:schema:load db:migrate
 bundle exec rake
 bundle exec brakeman -q --no-pager --ensure-latest
 bundle exec rubocop .
+./node_modules/karma/bin/karma start spec/javascripts/karma.conf.js
