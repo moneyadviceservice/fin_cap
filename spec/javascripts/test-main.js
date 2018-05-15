b6ddee03c6d8768ad711f7dc5099718124a7fc89
David Trussler <mail@davidtrussler.net>
2018-05-15 17:17:25 +0100

    Add Tooltip component to Styleguide

--- spec/javascripts/test-main.js
+++ spec/javascripts/test-main.js
@@ -38,6 +38,7 @@ require.config({
 
     // Components
     Nav: 'app/assets/javascripts/components/Nav',
-    Nav_A11Y: 'app/assets/javascripts/components/Nav_A11Y'
+    Nav_A11Y: 'app/assets/javascripts/components/Nav_A11Y',
+    Tooltip: 'app/assets/javascripts/components/Tooltip'
   }
 })
