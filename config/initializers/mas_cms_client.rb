e7cf5e00e2081d95673aa2d6e79dc44bb9e395c3
Will Hall <willhall88@gmail.com>
2018-05-23 12:01:23 +0100

    Update mas_cms_client setup var to use FRONTEND_REQUEST_TIMEOUT
    This could well be a temporary fix. Mainly to try to get
    fincap preview up and working as it currently is missing the
    timeout variable. By using the same as Frontend it means not having
    to add another env variable to the build.
    
    Improvements will come on the mas-cms-client. Where a default timeout
    should be set. Errors should also be improved as this was not directly
    obvious that the missing variable was the reason of the error

--- config/initializers/mas_cms_client.rb
+++ config/initializers/mas_cms_client.rb
@@ -1,6 +1,6 @@
 Mas::Cms::Client.config do |c|
-  c.timeout = ENV['HTTP_REQUEST_TIMEOUT'].to_i
-  c.open_timeout = ENV['HTTP_REQUEST_TIMEOUT'].to_i
+  c.timeout = ENV['FRONTEND_HTTP_REQUEST_TIMEOUT'].to_i
+  c.open_timeout = ENV['FRONTEND_HTTP_REQUEST_TIMEOUT'].to_i
   c.host = ENV['FINCAP_CMS_URL']
   c.api_token = ENV['MAS_CMS_API_TOKEN']
   c.retries = 1
