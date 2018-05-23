a2db3bd9398e0a5b36706e771244ab03646cf0f8
Rhian Watkins <rhian.watkins@moneyadviceservice.org.uk>
2018-05-23 11:13:43 +0100

    Moved the click/hover area to encompas the entire component so users can interact with content in tooltip
    Also changed to mousenter/leave to fix bug where on hover on,off,on were getting called

--- app/assets/javascripts/components/Tooltip.js
+++ app/assets/javascripts/components/Tooltip.js
@@ -7,7 +7,6 @@ define(['jquery', 'DoughBaseComponent'], function($, DoughBaseComponent) {
   Tooltip = function($el, config) {
     Tooltip.baseConstructor.call(this, $el, config, defaultConfig);
 
-    this.$button = $el.find('[data-tooltip-button]');
     this.$content = $el.find('[data-tooltip-content]');
     this.hiddenClass = 'js-is-hidden';
   };
@@ -34,24 +33,24 @@ define(['jquery', 'DoughBaseComponent'], function($, DoughBaseComponent) {
     // Default - show on click, hide on blur/second click
     /* If mouse events aren't supported or if the user clicks while still "hovering" then we use click.
     The pause variable makes sure if the mouseover event is triggered then click is not. */
-    this.$button.on('click', function() {
+    this.$el.on('click', function() {
       if (!pause){
         self.$content.toggleClass(self.hiddenClass);
       }
       pause = false;
     });
     /* In case the user clicks elsewhere on the page and mouseout hasn't fired */
-    this.$button.on('blur', function() {
+    this.$el.on('blur', function() {
       self.$content.addClass(self.hiddenClass);
     });
 
     // Hover supported - show on hover
-    this.$button.on('mouseover', function() {
+    this.$el.on('mouseenter', function() {
       pause = true;
       self.$content.removeClass(self.hiddenClass);
     });
 
-    this.$button.on('mouseout', function() {
+    this.$el.on('mouseleave', function() {
       self.$content.addClass(self.hiddenClass);
     });
   };
