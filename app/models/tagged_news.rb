d867e1e4c5ca039c0686d4d9b74ce5a726b68dda
Margo Urey <margo@margonline.co.uk>
2018-07-13 22:43:09 +0100

    Set a default number of news items to show

--- app/models/tagged_news.rb
+++ app/models/tagged_news.rb
@@ -1,7 +1,7 @@
 class TaggedNews
   def self.all(article)
     news_items = Mas::Cms::News.all(params: { tag: Array(article.tags) })
-                 
+
     news_items.reject { |news_item| news_item.slug == article.slug }
   end
 end
