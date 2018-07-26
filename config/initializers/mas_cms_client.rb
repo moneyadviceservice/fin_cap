5ce738213b71263e7510efae617b97498265ed0e
Tomas D'Stefano <tomas_stefano@successoft.com>
2018-07-26 17:15:57 +0100

    Use the right environment var name for cms api token
    After the work done on https://github.com/moneyadviceservice/cms/pull/459
    the api token for fincap was changed.

--- config/initializers/mas_cms_client.rb
+++ config/initializers/mas_cms_client.rb
@@ -2,7 +2,7 @@ Mas::Cms::Client.config do |c|
   c.timeout = ENV['FRONTEND_HTTP_REQUEST_TIMEOUT'].to_i
   c.open_timeout = ENV['FRONTEND_HTTP_REQUEST_TIMEOUT'].to_i
   c.host = ENV['FINCAP_CMS_URL']
-  c.api_token = ENV['MAS_CMS_API_TOKEN']
+  c.api_token = ENV['FINCAP_CMS_API_TOKEN']
   c.retries = 1
   c.cache = Rails.cache
   c.cache_gets = false
