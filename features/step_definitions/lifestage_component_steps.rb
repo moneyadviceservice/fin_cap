3b053ef58f28785de68c47273ba2a403270bbe9c
Margo Urey <margo@margonline.co.uk>
2018-08-16 16:35:29 +0100

    Update links for lifestages:
    - in translation file, add the slug of the article for each lifestage
    - re-record some cassettes as lifestage pages were created in the cms for test purposes

---
+++ features/step_definitions/lifestage_component_steps.rb
@@ -0,0 +1,25 @@
+Given('I entered a page which includes the lifestage component') do
+  step %(I entered into the Homepage)
+end
+
+When('I click on {string} in the lifestage component') do |lifestage|
+  lifestage_link = current_page.lifestage_rows.select do |row|
+    row.text == lifestage
+  end.first.link
+
+  lifestage_link.click
+end
+
+Then('I should see each lifestage with a link to its page') do |table|
+  lifestages = current_page.lifestage_rows
+
+  expect(lifestages.count).to eq(7)
+  table.rows.each do |row|
+    expect(row[0]).to be_in(lifestages.map(&:text))
+    expect(row[1]).to be_in(lifestages.map { |ls| ls.link[:href] })
+  end
+end
+
+Then('I should see the {string} page') do |string|
+  expect(current_page.title.text).to eq(string)
+end
