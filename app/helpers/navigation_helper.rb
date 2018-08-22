f533c0d69986139c935494795a5d3bde0a8563ea
David Trussler <mail@davidtrussler.net>
2018-08-22 17:45:06 +0100

    Comment: defines render_level_2_heading and adds to category partials

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -2,4 +2,8 @@ module NavigationHelper
   def render_level_1_heading(text)
     render 'navigation/level_1_heading', text: text
   end
+
+  def render_level_2_heading(text)
+    render 'navigation/level_2_heading', text: text
+  end
 end
