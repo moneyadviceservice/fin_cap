db1eeb9bfab4970d2b6800c2857ce5ed976c02c5
David Trussler <mail@davidtrussler.net>
2018-08-24 12:47:20 +0100

    Comment: Defines render_level_3_heading and adds to category partials

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -18,4 +18,12 @@ module NavigationHelper
   def render_level_2_subcategory_link(text, link)
     render 'navigation/level_2_subcategory_link', text: text, link: link
   end
+
+  def render_level_3_heading(text)
+    render 'navigation/level_3_heading', text: text
+  end
+
+  def render_level_3_subcategory_btn(text, link)
+    render 'navigation/level_3_subcategory_btn', text: text, link: link
+  end
 end
