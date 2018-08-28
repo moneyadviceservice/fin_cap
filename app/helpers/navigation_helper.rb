fb5ca6a7b882f8e2f0b876b4fb13314b53e5d477
David Trussler <mail@davidtrussler.net>
2018-08-28 15:49:33 +0100

    Comment: Adds constants for Fin Cap item

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,4 +1,21 @@
 module NavigationHelper
+  WHAT_FIN_CAP = {
+    'Financial capability levels in the UK' => Rails.application.routes.url_helpers.article_path('en', 'levels-uk'),
+    'Financial capability - a global issue' => Rails.application.routes.url_helpers.article_path('en', 'global-issue')
+  }.freeze
+
+  WHO_FOR = {
+    'Financial services' => Rails.application.routes.url_helpers.article_path('en', 'financial-services'),
+    'Charities and third-sector organisations' => Rails.application.routes.url_helpers.article_path('en', 'charities-third-sector-organisations'),
+    'Employers' => Rails.application.routes.url_helpers.article_path('en', 'employers'),
+    'Researchers' => Rails.application.routes.url_helpers.article_path('en', 'researchers'),
+    'Schools' => Rails.application.routes.url_helpers.article_path('en', 'schools'),
+    'Parents' => Rails.application.routes.url_helpers.article_path('en', 'parents'),
+    'Childrens services' => Rails.application.routes.url_helpers.article_path('en', 'childrens-services'),
+    'Youth and students' => Rails.application.routes.url_helpers.article_path('en', 'youth-students'),
+    'Individuals' => Rails.application.routes.url_helpers.article_path('en', 'individuals')
+  }
+
   def render_level_1_heading(text)
     render 'navigation/level_1_heading', text: text
   end
