1ed72aeebed69fa051f3aa161293368e8377bd07
David Trussler <mail@davidtrussler.net>
2018-08-29 17:28:35 +0100

    Comment: Adds values to translation file for "What Is Financial Capability?" item

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,20 +1,20 @@
 module NavigationHelper
-  WHAT_FIN_CAP = {
-    'Financial capability levels in the UK' => Rails.application.routes.url_helpers.article_path('en', 'levels-uk'),
-    'Financial capability - a global issue' => Rails.application.routes.url_helpers.article_path('en', 'global-issue')
-  }.freeze
+  # WHAT_FIN_CAP = {
+  #   'Financial capability levels in the UK' => Rails.application.routes.url_helpers.article_path('en', 'levels-uk'),
+  #   'Financial capability - a global issue' => Rails.application.routes.url_helpers.article_path('en', 'global-issue')
+  # }.freeze
 
-  WHO_FOR = {
-    'Financial services' => Rails.application.routes.url_helpers.article_path('en', 'financial-services'),
-    'Charities and third-sector organisations' => Rails.application.routes.url_helpers.article_path('en', 'charities-third-sector-organisations'),
-    'Employers' => Rails.application.routes.url_helpers.article_path('en', 'employers'),
-    'Researchers' => Rails.application.routes.url_helpers.article_path('en', 'researchers'),
-    'Schools' => Rails.application.routes.url_helpers.article_path('en', 'schools'),
-    'Parents' => Rails.application.routes.url_helpers.article_path('en', 'parents'),
-    'Childrens services' => Rails.application.routes.url_helpers.article_path('en', 'childrens-services'),
-    'Youth and students' => Rails.application.routes.url_helpers.article_path('en', 'youth-students'),
-    'Individuals' => Rails.application.routes.url_helpers.article_path('en', 'individuals')
-  }
+  # WHO_FOR = {
+  #   'Financial services' => Rails.application.routes.url_helpers.article_path('en', 'financial-services'),
+  #   'Charities and third-sector organisations' => Rails.application.routes.url_helpers.article_path('en', 'charities-third-sector-organisations'),
+  #   'Employers' => Rails.application.routes.url_helpers.article_path('en', 'employers'),
+  #   'Researchers' => Rails.application.routes.url_helpers.article_path('en', 'researchers'),
+  #   'Schools' => Rails.application.routes.url_helpers.article_path('en', 'schools'),
+  #   'Parents' => Rails.application.routes.url_helpers.article_path('en', 'parents'),
+  #   'Childrens services' => Rails.application.routes.url_helpers.article_path('en', 'childrens-services'),
+  #   'Youth and students' => Rails.application.routes.url_helpers.article_path('en', 'youth-students'),
+  #   'Individuals' => Rails.application.routes.url_helpers.article_path('en', 'individuals')
+  # }
 
   WHAT_IS_STRATEGY = {
     'UK Strategy' => '/uk_strategies/uk-strategy',
